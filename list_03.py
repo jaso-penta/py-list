bb1f0eef41082b7a4aabef7b609d23b7ce56241f
Jašo <andrija.pericic@gmail.com>
2025-09-11 20:09:47 +0200

    Create list_03.py

---
+++ list_03.py
@@ -0,0 +1,10 @@
+names = ['Josip Jelacic', 'Nikola Zrinski', 'Juraj Dobrila']
+
+drugi_element = names[1]
+#print(names[3]) greska
+
+print(drugi_element)
+
+names.append('Josip Strossmayer')
+print(names)
+print(names[3])
