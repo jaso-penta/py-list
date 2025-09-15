3132e753d3088716b3971cfb9aceed5190a7b231
Jašo <andrija.pericic@gmail.com>
2025-09-15 19:07:14 +0200

    Create list_05.py

---
+++ list_05.py
@@ -0,0 +1,15 @@
+# insert in list
+
+
+# nacin da range konvertiramo u listu
+numbers = list(range(10))
+
+print(numbers)
+
+# dodaje element na posljednje mjesto u listi
+numbers.append(11)
+print(numbers)
+
+#
+numbers.insert(4, 'jedanaest')
+print(numbers)
