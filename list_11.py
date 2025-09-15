bd6176943ca5476fb4f80ff4fbd140e7ddaacdb9
Jašo <andrija.pericic@gmail.com>
2025-09-15 20:11:28 +0200

    Update list_11.py

--- list_11.py
+++ list_11.py
@@ -1,10 +1,9 @@
-#slice notacija
+# slice notacija
 numbers = list(range(21))
-print(numbers)
+# print(numbers)
 
-
-numbers_copy = numbers
-print(numbers_copy)
+# numbers_copy = numbers
+# print(numbers_copy)
 
 # numbers.append(21)
 # print(numbers)
@@ -16,12 +15,27 @@ print(numbers_copy)
 
 print()
 print('1. nacin')
+print()
 numbers_copy = numbers.copy()
 
-numbers_copy.append(23)
+numbers.append(23)
 print(numbers)
 print(numbers_copy)
 
 numbers_copy.append(24)
+print(numbers)
+print(numbers_copy)
+
+
+print()
+print('2. nacin')
+print()
+numbers_copy = numbers[ : : ]
+
+numbers.append(31)
+print(numbers)
+print(numbers_copy)
+
+numbers_copy.append(32)
 print(numbers)
 print(numbers_copy)
